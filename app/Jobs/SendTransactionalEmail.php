baeb04b20d33566ba1d9825175de2845114f5eb8
Pavel Grancharov <dvdtygc@gmail.com>
2021-04-12 02:45:40 +0300

    Log emails on success/fail

--- app/Jobs/SendTransactionalEmail.php
+++ app/Jobs/SendTransactionalEmail.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\OutboundEmail;
 use App\Services\Email\Email;
 use App\Services\Email\EmailApiManager;
 use App\Services\Email\NoDriversLeftException;
@@ -25,14 +26,25 @@ class SendTransactionalEmail implements ShouldQueue
     public function handle(EmailApiManager $emailApiManager): bool
     {
         foreach ($emailApiManager->getDrivers() as $driver) {
+            $outboundEmail = new OutboundEmail();
+
+            $outboundEmail->success = true;
+            $outboundEmail->driver = $driver;
+            $outboundEmail->recipient = $this->recipient;
+            $outboundEmail->subject = $this->email->getSubject();
+            $outboundEmail->content = $this->email->getContent();
+
             try {
                 $emailApiManager->driver($driver)->send($this->email, $this->recipient);
 
-                // log
+                $outboundEmail->save();
 
                 return true;
             } catch (Throwable $exception) {
-                // log
+                $outboundEmail->success = false;
+                $outboundEmail->error_message = $exception->getMessage();
+
+                $outboundEmail->save();
             }
         }
 
