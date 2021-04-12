448618efefde95dc1a0ace37a79c43795c316106
Pavel Grancharov <dvdtygc@gmail.com>
2021-04-12 13:21:52 +0300

    Add tests for queuing job, sending it through external service, sending it through fallback external service when another one is failing

--- tests/Feature/TransactionalEmailTest.php
+++ tests/Feature/TransactionalEmailTest.php
@@ -2,7 +2,15 @@
 
 namespace Tests\Feature;
 
+use App\Jobs\SendTransactionalEmail;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Bus;
+use Mailjet\Client as MailjetClient;
+use Mailjet\Response as MailjetResponse;
+use Mockery;
+use Mockery\MockInterface;
+use SendGrid as SendgridClient;
+use SendGrid\Response as SendgridResponse;
 use Tests\TestCase;
 
 class TransactionalEmailTest extends TestCase
@@ -12,7 +20,7 @@ class TransactionalEmailTest extends TestCase
     /**
      * @test
      */
-    public function it_validates_required_fields_on_post_request()
+    public function it_validates_required_fields()
     {
         $this->postJson(route('send-transactional-emails'))
             ->assertStatus(422)
@@ -26,19 +34,109 @@ class TransactionalEmailTest extends TestCase
     /**
      * @test
      */
-    public function it_validates_recipients_are_valid_emails_on_post_request()
+    public function it_validates_recipients_are_valid_emails()
     {
-        $this->postJson(route('send-transactional-emails'), [
-            'type'       => 'text',
-            'recipients' => ['invalid-email'],
-            'subject'    => 'Subject',
-            'content'    => ['Line 1', 'Line 2'],
-        ])
+        $this->postJson(
+            route('send-transactional-emails'),
+            $this->getEmailData(['recipients' => ['invalid-email']]),
+        )
             ->assertStatus(422)
             ->assertJson(['message' => 'The given data was invalid.'])
             ->assertJsonStructure([
                 'message',
                 'errors' => ['recipients.0'],
             ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_adds_emails_to_the_queue()
+    {
+        Bus::fake();
+
+        $this->postJson(route('send-transactional-emails'), $this->getEmailData())
+            ->assertStatus(202);
+
+        Bus::assertDispatched(SendTransactionalEmail::class);
+    }
+
+    /**
+     * @test
+     */
+    public function it_sends_emails_through_external_service()
+    {
+        $this->mock(SendgridClient::class, function (MockInterface $mock) {
+            $mock->shouldReceive('send')
+                ->once()
+                ->andReturn(new SendgridClient\Response(202));
+        });
+
+        $this->postJson(route('send-transactional-emails'), $this->getEmailData());
+
+        $this->assertDatabaseCount('outbound_emails', 1);
+
+        $this->assertDatabaseHas('outbound_emails', [
+            'success'       => 1,
+            'recipient'     => $this->getEmailData()['recipients'][0],
+            'subject'       => $this->getEmailData()['subject'],
+            'error_message' => null,
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_sends_emails_through_fallback_external_service()
+    {
+        $errorMessage = 'Invalid request';
+
+        $this->mock(SendgridClient::class, function (MockInterface $mock) use ($errorMessage) {
+            $sendgridResponse = Mockery::mock(SendgridResponse::class);
+
+            $sendgridResponse->shouldReceive('statusCode')->once()->andReturn(400);
+
+            $sendgridResponse->shouldReceive('body')->once()->andReturn($errorMessage);
+
+            $mock->shouldReceive('send')->once()->andReturn($sendgridResponse);
+        });
+
+        $this->mock(MailjetClient::class, function (MockInterface $mock) {
+            $mailjetResponse = Mockery::mock(MailjetResponse::class);
+
+            $mailjetResponse->shouldReceive('success')->once()->andReturn(true);
+
+            $mailjetResponse->shouldReceive('getStatus')->once()->andReturn(200);
+
+            $mock->shouldReceive('post')->once()->andReturn($mailjetResponse);
+        });
+
+        $this->postJson(route('send-transactional-emails'), $this->getEmailData());
+
+        $this->assertDatabaseCount('outbound_emails', 2);
+
+        $this->assertDatabaseHas('outbound_emails', [
+            'success'       => 0,
+            'recipient'     => $this->getEmailData()['recipients'][0],
+            'subject'       => $this->getEmailData()['subject'],
+            'error_message' => $errorMessage,
+        ]);
+
+        $this->assertDatabaseHas('outbound_emails', [
+            'success'       => 1,
+            'recipient'     => $this->getEmailData()['recipients'][0],
+            'subject'       => $this->getEmailData()['subject'],
+            'error_message' => null,
+        ]);
+    }
+
+    protected function getEmailData(array $attributes = []): array
+    {
+        return array_merge([
+            'type'       => 'text',
+            'recipients' => ['example@example.com'],
+            'subject'    => 'Subject',
+            'content'    => ['Line 1', 'Line 2'],
+        ], $attributes);
     }
 }
