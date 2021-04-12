7c62b87ad6c26b5552325017f04c82b6e4e513fd
Pavel Grancharov <dvdtygc@gmail.com>
2021-04-12 10:42:05 +0300

    Add test for validating recipients are valid emails

--- tests/Feature/TransactionalEmailTest.php
+++ tests/Feature/TransactionalEmailTest.php
@@ -12,14 +12,33 @@ class TransactionalEmailTest extends TestCase
     /**
      * @test
      */
-    public function it_validates_post_request()
+    public function it_validates_required_fields_on_post_request()
     {
         $this->postJson(route('send-transactional-emails'))
             ->assertStatus(422)
             ->assertJson(['message' => 'The given data was invalid.'])
             ->assertJsonStructure([
                 'message',
-                'errors' => ['type', 'recipients', 'subject', 'content']
+                'errors' => ['type', 'recipients', 'subject', 'content'],
+            ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_validates_recipients_are_valid_emails_on_post_request()
+    {
+        $this->postJson(route('send-transactional-emails'), [
+            'type'       => 'text',
+            'recipients' => ['invalid-email'],
+            'subject'    => 'Subject',
+            'content'    => ['Line 1', 'Line 2'],
+        ])
+            ->assertStatus(422)
+            ->assertJson(['message' => 'The given data was invalid.'])
+            ->assertJsonStructure([
+                'message',
+                'errors' => ['recipients.0'],
             ]);
     }
 }
